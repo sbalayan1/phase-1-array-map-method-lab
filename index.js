6ecc644592ae9c3c25766cb3b37851296dbd7e80
sbalayan1 <balayans2014@yahoo.com>
2021-05-24 01:42:00 -0700

    commit tree 65f0b677c33c3c5d437dbc4193fadc24bf81c40e

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
-
-const titleCased = () => {
-  return tutorials
+const titleCased = function titleCased(someArray) {
+  let mapArray = someArray.map(function (element) {
+    let adjustElement = element.split(" ").map(word => word[0].toUpperCase() + word.substr(1)).join(" ")
+    
+    return adjustElement;
+  })
+    return mapArray
 }
+
+titleCased(tutorials)
+
+
+
+//split only works on a single string at a time. you have to .split each element of the tutorials array one by one. 
+//.join -> lets you join an array. only works on arrays. Without the "spaces", it would just create an array with - dashes. 
+// .map is good when you need the same number of outputs 
+//what does substr create? -> essentially the number you give it is an index starting point of the string and returns the rest of the string from that starting point
+
+//const tutorials = 'what does the this keyword mean?',
+//console.log(tutorials.split(" "))
+//const words = words.split(" ")
+
+//the below works for splitting, iterating through, uppercasing, and rejoining a string. 
+//tutorials.split('').map(word => word[0].toUpperCase + word.substr(1)).join(" ")
+
+//tips.map()
+
+//broken down version of running the .split and .map function 
+//function titleCase(someString) {
+  //let sentence = someString.toLowerCase().split(" ")
+  //for (i = 0; i < sentence.length; i++) {
+    //sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+  //}
+  //return sentence
+//}
+
